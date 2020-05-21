98c910b4b7b2bc22471b9ae10d31605ae4ae7c80
zachariapopcorn <52849920+zachariapopcorn@users.noreply.github.com>
2020-05-21 04:31:10 -0700

    Update setprefix.js

--- setprefix.js
+++ setprefix.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 
 exports.run = (client, message, args) => {
+    if(message.author.id != "YOUR DISCORD ID HERE") {
+        return message.channel.send("You don't have permission to run this command!");
+    }
     let prefix = args[0];
     if(!prefix) {
         return message.channel.send("Please enter a new prefix!");
