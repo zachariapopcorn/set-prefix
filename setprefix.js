599ca0423b171855292440c705d6e6aa5230f7a0
zachariapopcorn <52849920+zachariapopcorn@users.noreply.github.com>
2020-06-24 11:23:51 -0700

    Update setprefix.js

--- setprefix.js
+++ setprefix.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+require('dotenv').config();
 
 exports.run = (client, message, args) => {
     if(message.author.id != "YOUR DISCORD ID HERE") {
@@ -8,13 +8,6 @@ exports.run = (client, message, args) => {
     if(!prefix) {
         return message.channel.send("Please enter a new prefix!");
     }
-    let configFile = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
-    configFile.prefix = prefix;
-    client.config.prefix = prefix;
-    fs.writeFile('./config.json', JSON.stringify(configFile), (err) => {
-        if(err) {
-            message.channel.send("There was an error while setting the prefix:" + err);
-        }
-    });
+    process.env.prefix = prefix;
     return message.channel.send("Successfully changed the prefix to " + prefix);
 }
